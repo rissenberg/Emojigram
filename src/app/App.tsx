43929bb7b6ce836bd66246dbc5e422263ae5b0fc
rissenberg <rissychannel@gmail.com>
2024-03-31 23:05:06 +0300

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        Hello from Emojigram!
+      </div>
+    </div>
+  );
+}
+
+export default App;
